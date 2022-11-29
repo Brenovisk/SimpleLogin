fbfb586b8a9a19d42c771497c9e1a8046a3dae9d
Breno Lucas <breno.lucas@mc1global.com>
2022-11-29 20:41:06 -0300

    validation email login

--- login.js
+++ login.js
@@ -7,49 +7,54 @@ var attempt = 3;
 buttonLogin.addEventListener("click", validate);
 
 function validate(event) {
-    event.preventDefault(); //Previne recarregamento da página
+  event.preventDefault(); //Previne recarregamento da página
 
-    var email = document.getElementById("email").value;
-    var pwd = document.getElementById("password").value;
-    var message = document.getElementById("message");
-    var hasAttempt = attempt !== 0;
+  var email = document.getElementById("email").value;
+  var pwd = document.getElementById("password").value;
+  var message = document.getElementById("message");
+  var hasAttempt = attempt !== 0;
 
-    if (email == "" || pwd == "") {
-        message.textContent = "Preencha todos os campos!";
-    } else if (email == defaultUser && pwd == defaultPwd && hasAttempt) {
-        window.location = "initialPage.html"; // Redireciona para página principal.
-        return false;
-    } else {
-        attempt--; // Decrementa tentativas.
+  if (email == "" || pwd == "") {
+    message.textContent = "Preencha todos os campos!";
+  } else if (
+    email.includes("@") &&
+    email == defaultUser &&
+    pwd == defaultPwd &&
+    hasAttempt
+  ) {
+    window.location = "initialPage.html"; // Redireciona para página principal.
+    return false;
+  } else {
+    attempt--; // Decrementa tentativas.
 
-        var msg1 = "Você tem apenas mais " + attempt + " tentativa!";
-        var msg2 =
-            "Suas tentativas acabaram! Recarregue a página e tente novamente.";
+    var msg1 = "Você tem apenas mais " + attempt + " tentativa!";
+    var msg2 =
+      "Suas tentativas acabaram! Recarregue a página e tente novamente.";
 
-        message.textContent = msg1;
+    message.textContent = msg1;
 
-        // Desabilita os campos após 3 tentativas.
-        if (attempt === 0) {
-            message.textContent = msg2;
-            document.getElementById("email").disabled = true;
-            document.getElementById("password").disabled = true;
-            document.getElementById("button").disabled = true;
-            return false;
-        }
+    // Desabilita os campos após 3 tentativas.
+    if (attempt === 0) {
+      message.textContent = msg2;
+      document.getElementById("email").disabled = true;
+      document.getElementById("password").disabled = true;
+      document.getElementById("button").disabled = true;
+      return false;
     }
+  }
 }
 
 //Adiciona estilos diferente aos inputs, caso esse tenha ou não valor
 let inputs = document.getElementsByClassName("input-form");
 
 for (let input of inputs) {
-    function transparentText() {
-        var value = input.value.trim();
+  function transparentText() {
+    var value = input.value.trim();
 
-        value != "" ?
-            input.classList.add("has-val") :
-            input.classList.remove("has-val");
-    }
+    value != ""
+      ? input.classList.add("has-val")
+      : input.classList.remove("has-val");
+  }
 
-    input.addEventListener("blur", transparentText);
+  input.addEventListener("blur", transparentText);
 }
